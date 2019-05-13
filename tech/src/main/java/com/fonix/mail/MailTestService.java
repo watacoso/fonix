9c9e567f7350a39dac1e6a2231757872bd325111
Guido Pintus <g.pintus@vargroup.it>
2019-05-14 01:07:39 +0200

    Change in format of simulated mails

--- tech/src/main/java/com/fonix/mail/MailTestService.java
+++ tech/src/main/java/com/fonix/mail/MailTestService.java
@@ -13,7 +13,7 @@ public class MailTestService implements MailService{
     public void send(String email, String message) {
 
 
-        String s=String.format("\nTo: %s \n\n %s ",email,message);
+        String s=String.format("\n\nTo: %s \n\n %s \n\n ",email,message);
 
         logger.info(s);
     }
