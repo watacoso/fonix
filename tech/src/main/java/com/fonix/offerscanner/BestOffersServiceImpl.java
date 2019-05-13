7c74e2bb56b6ebaca3793cc0e38046e9de8bee78
Guido Pintus <g.pintus@vargroup.it>
2019-05-14 01:09:42 +0200

    Implementation of BestOffersService. It retrieves all Observers to be notified. then, transactionally, it updates their status on the database, and notifies the observer with a mail

--- tech/src/main/java/com/fonix/offerscanner/BestOffersServiceImpl.java
+++ tech/src/main/java/com/fonix/offerscanner/BestOffersServiceImpl.java
@@ -1,9 +1,18 @@
 package com.fonix.offerscanner;
 
+import com.fonix.db.dao.ObserverDAO;
 import com.fonix.db.dao.ObserversFlightsDAO;
+import com.fonix.util.Frequency;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,11 +24,49 @@ public class BestOffersServiceImpl implements BestOffersService {
     @Autowired
     private ObserversFlightsDAO observersFlightsDAO;
 
+    @Autowired
+    ObserverDAO observerDAO;
+
     @Override
     public void deliverBestOffers() {
         List<OfferModel> bestOffers=observersFlightsDAO.getBestOffersForObservers();
 
-        bestOffers.forEach(notifyObserverService::sendNotification);
+        bestOffers.forEach(this::contactObserver);
+
+    }
+
+    @Transactional
+    protected void contactObserver(OfferModel offerModel){
+
+        BigDecimal prevOffer=offerModel.getPreviousBestOffer();
+        BigDecimal currentOffer=offerModel.getCurrentBestOffer();
+        LocalDateTime dateTime=LocalDateTime.ofInstant(offerModel.getDepartureTime().toInstant(), ZoneId.systemDefault());
+
+        Frequency frequency=offerModel.getFrequency();
+
+        switch (frequency){
+            case DAILY:
+                dateTime= dateTime.plusDays(1);
+                break;
+            case WEEKLY:
+                dateTime=dateTime.plusWeeks(1);
+                break;
+            case MONTHLY:
+                dateTime=dateTime.plusMonths(1);
+                break;
+        }
+
+        Date nextUpdate= Date.from(dateTime
+                .atZone(ZoneId.systemDefault())
+                .toInstant());
+
+        observerDAO.updateStatus(offerModel.getEmail(),
+                offerModel.getCurrentBestOffer(),
+                nextUpdate);
+
+        if(prevOffer==null ||!currentOffer.equals(prevOffer)){
+            notifyObserverService.sendNotification(offerModel);
+        }
 
     }
 
