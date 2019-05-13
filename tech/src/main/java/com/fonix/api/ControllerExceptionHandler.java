a35eea4b183309daae58e93d3b63813c96fddf48
Guido Pintus <g.pintus@vargroup.it>
2019-05-13 22:03:25 +0200

    first addition of error logs

--- tech/src/main/java/com/fonix/api/ControllerExceptionHandler.java
+++ tech/src/main/java/com/fonix/api/ControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.fonix.api;
 
 
+import com.fonix.mail.MailTestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +17,13 @@ import org.springframework.web.util.WebUtils;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
     @ExceptionHandler({RuntimeException.class})
     public final ResponseEntity<ApiError> handleException(Exception ex, WebRequest req){
 
         HttpStatus status=HttpStatus.INTERNAL_SERVER_ERROR;
         ApiError body=new ApiError("Generic error");
-
+        logger.error(ex.getMessage());
         return buildResponse(ex,status,body,req);
 
     }
