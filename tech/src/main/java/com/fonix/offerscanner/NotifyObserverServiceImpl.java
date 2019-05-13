01629e91aae0b18b8bd6cd3639f923924f34ed92
Guido Pintus <g.pintus@vargroup.it>
2019-05-14 01:10:15 +0200

    Implementation of update mail sender

--- tech/src/main/java/com/fonix/offerscanner/NotifyObserverServiceImpl.java
+++ tech/src/main/java/com/fonix/offerscanner/NotifyObserverServiceImpl.java
@@ -2,16 +2,56 @@ package com.fonix.offerscanner;
 
 //Transactionally notify an obsterver and update it's status in the database
 
+import com.fonix.mail.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class NotifyObserverServiceImpl implements NotifyObserverService{
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 
+@Service
+public class NotifyObserverServiceImpl implements NotifyObserverService {
+
+
+    @Autowired
+    MailService mailService;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy HH:mm");
 
     @Override
-    @Transactional
     public void sendNotification(OfferModel offerModel) {
 
+        String msg = buildMessage(offerModel);
+        mailService.send(offerModel.getEmail(), msg);
+
+    }
+
+    private String buildMessage(OfferModel offerModel) {
+
+        String from = offerModel.getOriginCode();
+        String to = offerModel.getDestinationCode();
+
+
+        String currentOffer = offerModel.getCurrentBestOffer().toPlainString();
+
+        String flightCode = offerModel.getFlightCode();
+        String dateTime = sdf.format(offerModel.getDepartureTime());
+
+        String priceChangeString=String.format("The price is now %s !\n\n", currentOffer);
+        if(offerModel.getPreviousBestOffer()!=null){
+            String prevOffer = offerModel.getPreviousBestOffer().toPlainString();
+            priceChangeString=String.format("The price has gone changed, From %s to %s !\n\n", prevOffer, currentOffer);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Update on flights from %s to %s!\n", from, to))
+                .append(priceChangeString)
+                .append(String.format("Flight code: %s", flightCode))
+                .append(String.format("\nDeparture: %s", dateTime))
+                .append("\n\nRegards,")
+                .append("\n\nFonix");
+
+        return sb.toString();
     }
 }
