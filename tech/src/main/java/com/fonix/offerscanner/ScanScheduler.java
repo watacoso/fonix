f990b6547f6c4fd5579fb36bcac3e66697e39e7f
Guido Pintus <g.pintus@vargroup.it>
2019-05-14 02:30:56 +0200

    Parametized scheduling time

--- tech/src/main/java/com/fonix/offerscanner/ScanScheduler.java
+++ tech/src/main/java/com/fonix/offerscanner/ScanScheduler.java
@@ -1,6 +1,7 @@
 package com.fonix.offerscanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +11,14 @@ public class ScanScheduler {
     @Autowired
     BestOffersService bestOffersService;
 
+    /*
+    *
+    * For the case of uncapped observers, i decided for an approach that unbounds the
+    * insertion of a new flight or observer from the scan operation, at the loss of
+    * instant updates regarding the offers.
+    * */
 
-
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRateString = "${scanner.scheduler.interval.millis}")
     public void execute() {
         bestOffersService.deliverBestOffers();
     }
